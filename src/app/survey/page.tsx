3627efbd7c51ac39e31a99cf9012608df194bacc
Arsyadam <work@arsyadam.id>
2024-02-27 07:55:30 +0700

    Update Survey

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -1,12 +1,19 @@
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Survey Fashion by TheNextWay',
+  description: 'Haloo✨Terimakasih sudah menyempatkan waktu kalian untuk mengisi survey kami!',
+}
+
 export default function Survey() {
     return (
       <>
-      <div className="mx-auto max-w-screen-xl p-5">
+      <div className="mx-auto max-w-screen-xl p-5 text-center" >
 
             <h1 className="font-bold text-[50px]">Survey Fashion</h1>
       </div>
         <div className='flex justify-center'>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeY5GDX-wogMcMrvW9kpqID5i6319aFzbn3uh38sP_QFCSKSg/viewform?embedded=true" width="640" height="4842" >Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeY5GDX-wogMcMrvW9kpqID5i6319aFzbn3uh38sP_QFCSKSg/viewform?embedded=true" width="640" height="7800" >Loading…</iframe>
         </div>
       </>
     )
